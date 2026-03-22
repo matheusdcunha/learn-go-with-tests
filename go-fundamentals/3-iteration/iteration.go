a253bddb5a7453371a595efd3b416cb7f02797ab
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-03-22 14:07:21 -0300

    refactor: add strings.Builder to improve performance in iteration

--- go-fundamentals/3-iteration/iteration.go
+++ go-fundamentals/3-iteration/iteration.go
@@ -1,15 +1,17 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string) string {
-	var repeated string
+	var repeated strings.Builder
 
 	// for range 5 {
 	// 	repeated = repeated + character
 	// }
 
 	for i := 0; i < 5; i++ {
-		repeated += character
+		repeated.WriteString(character)
 	}
 
-	return repeated
+	return repeated.String()
 }
