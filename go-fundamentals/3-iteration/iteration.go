1e94362fec5351c77ff8f7503a072d52b381e9b9
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-03-22 13:56:33 -0300

    feat: green phase in interation

---
+++ go-fundamentals/3-iteration/iteration.go
@@ -0,0 +1,15 @@
+package iteration
+
+func Repeat(character string) string {
+	var repeated string
+
+	// for range 5 {
+	// 	repeated = repeated + character
+	// }
+
+	for i := 0; i < 5; i++ {
+		repeated = repeated + character
+	}
+
+	return repeated
+}
