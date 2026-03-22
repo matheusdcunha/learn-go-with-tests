b51044f659442a921a269c89dda349f1932ff0b0
Matheus da Cunha <matheus.cunha.fonseca50@gmail.com>
2026-03-22 14:12:45 -0300

    feat: add ExampleRepeat

--- go-fundamentals/3-iteration/iteration_test.go
+++ go-fundamentals/3-iteration/iteration_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 4)
@@ -9,6 +12,12 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expectd %q but got %q", expected, repeated)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
 
 func BenchmarkRepeat(b *testing.B) {
